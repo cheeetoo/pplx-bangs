9b0fe1158a9dbbbc62b79ba05d9bf725615ea15a
Finn Cairns <fjamescairns@gmail.com>
2024-02-03 14:03:33 -0600

    working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use warp::{http::StatusCode, hyper::Uri, Filter, Rejection, Reply};
 
 #[tokio::main]
@@ -13,23 +14,48 @@ async fn handle_search(
     params: std::collections::HashMap<String, String>,
 ) -> Result<Box<dyn Reply>, Rejection> {
     if let Some(query) = params.get("q") {
-        let base_url = "https://perplexity.ai/search?q={}";
-        let redirect_url = match query.split_whitespace().next().unwrap_or("") {
-            "!g" => "https://google.com/search?q={}",
-            _ => "",
+        let redirect_url = match query.chars().next().unwrap() {
+            '!' => get_bang(query).await.unwrap(),
+            _ => format!(
+                "https://perplexity.ai/search?q={}",
+                &urlencoding::encode(query)
+            ),
         };
-        let parsed_url: Uri = (match redirect_url.len() {
-            0 => base_url.replace("{}", &urlencoding::encode(query)),
-            _ => redirect_url.replace("{}", &urlencoding::encode(query.splitn(2, ' ').nth(1).unwrap_or(""))),
-        })
-        .parse()
-        .unwrap();
 
-        Ok(Box::new(warp::redirect::temporary(parsed_url)))
+        Ok(Box::new(warp::redirect::temporary(redirect_url.parse::<Uri>().unwrap())))
     } else {
         Ok(Box::new(warp::reply::with_status(
             "Query parameter 'q' is required.",
             StatusCode::BAD_REQUEST,
         )))
     }
+}
+
+async fn get_bang(query: &String) -> anyhow::Result<String> {
+    let re_first: Regex = Regex::new("url=([^']+)")?;
+    let re_last = Regex::new("https%3A%2F%2F([^&]+(?:&[^r]+)*)")?;
+    let response = &reqwest::get(
+        "https://duckduckgo.com/?q=".to_string() + &urlencoding::encode(query).into_owned(),
+    )
+    .await?
+    .text()
+    .await?;
+    let mut url = re_first
+        .captures(response)
+        .and_then(|c| c.get(1))
+        .map(|m| m.as_str().to_owned())
+        .unwrap();
+    if url.starts_with("/l/?uddg=") {
+        url = urlencoding::decode(&format!(
+            "https://{}",
+            re_last
+                .captures(url.as_str())
+                .and_then(|c| c.get(1))
+                .map(|m| m.as_str())
+                .unwrap()
+        ))?
+        .into_owned();
+    }
+
+    return Ok(url);
 }

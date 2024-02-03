96ae07ef819c90e5893e937e91f3c97495d23528
Finn Cairns <fjamescairns@gmail.com>
2024-02-03 17:59:46 -0600

    axum is cool

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,35 @@
+use axum::{extract::Query, response::Redirect, routing::get, Router};
 use regex::Regex;
-use warp::{http::StatusCode, hyper::Uri, Filter, Rejection, Reply};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct QueryParams {
+    q: String,
+}
 
 #[tokio::main]
 async fn main() {
-    let search_route = warp::path::end()
-        .and(warp::query::<std::collections::HashMap<String, String>>())
-        .and_then(handle_search);
-
-    warp::serve(search_route).run(([127, 0, 0, 1], 3030)).await;
+    let app = Router::new().route("/", get(root));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3030").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
 
-async fn handle_search(
-    params: std::collections::HashMap<String, String>,
-) -> Result<Box<dyn Reply>, Rejection> {
-    if let Some(query) = params.get("q") {
-        let redirect_url = match query.chars().next().unwrap() {
-            '!' => get_bang(query).await.unwrap(),
-            _ => format!(
-                "https://perplexity.ai/search?q={}",
-                &urlencoding::encode(query)
-            ),
-        };
-
-        Ok(Box::new(warp::redirect::temporary(redirect_url.parse::<Uri>().unwrap())))
-    } else {
-        Ok(Box::new(warp::reply::with_status(
-            "Query parameter 'q' is required.",
-            StatusCode::BAD_REQUEST,
-        )))
-    }
+async fn root(Query(params): Query<QueryParams>) -> Redirect {
+    let redirect_url = match params.q.chars().next().unwrap() {
+        '!' => get_bang(params.q).await.unwrap(),
+        _ => format!(
+            "https://perplexity.ai/search?q={}",
+            &urlencoding::encode(&params.q)
+        ),
+    };
+    Redirect::permanent(&redirect_url)
 }
 
-async fn get_bang(query: &String) -> anyhow::Result<String> {
+async fn get_bang(query: String) -> anyhow::Result<String> {
     let re_first: Regex = Regex::new("url=([^']+)")?;
     let re_last = Regex::new("https%3A%2F%2F([^&]+(?:&[^r]+)*)")?;
     let response = &reqwest::get(
-        "https://duckduckgo.com/?q=".to_string() + &urlencoding::encode(query).into_owned(),
+        "https://duckduckgo.com/?q=".to_string() + &urlencoding::encode(&query).into_owned(),
     )
     .await?
     .text()
